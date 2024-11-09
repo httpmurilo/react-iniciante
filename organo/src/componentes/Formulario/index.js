8ed00c0b58daadc71caa2c290ddb223851aa8bb2
httpmurilo <muriloeduardo1997@outlook.com>
2024-11-08 22:08:03 -0300

    ok

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -5,8 +5,9 @@ import CampoTexto from './componentes/CampoTexto';
 const formulario = () =>{
 
     return (
-       <section>
+       <section className="formulario">
         <form>
+        <h2>Preencha os dados para criar card do colaborador</h2>
         <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
