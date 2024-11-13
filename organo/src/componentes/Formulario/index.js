fdf9870a21f5c75a011c55e36284b9d91d5b2eda
httpmurilo <muriloeduardo1997@outlook.com>
2024-11-13 17:33:21 -0300

    campo texto

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 
 const formulario = () =>{
@@ -15,14 +16,22 @@ const formulario = () =>{
         'Inovação e Gestão'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+
+    }
     return (
        <section className="formulario">
-        <form>
+        <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-        <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-        <ListaSuspensa label="Time" itens={times}/>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome"/>
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"/>
+        <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da imagem"/>
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+        <Botao>
+            Criar Card
+        </Botao>
         </form>
        </section>
     )
