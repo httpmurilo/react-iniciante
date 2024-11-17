413202d43181f6cc4321e753e1f194f8a5456dbe
httpmurilo <muriloeduardo1997@outlook.com>
2024-11-17 13:30:54 -0300

    encerração do projeto

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,11 +1,14 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 function Banner() {
     //jsx
     return (
+        <Fragment>
         <header className="banner">
             <img src="/images/banner.png" alt="banner principal da pagina" />
         </header>
+        </Fragment>
         
     )
 }
